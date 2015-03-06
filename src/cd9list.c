387dd3bf845490b0fd1be4b09a1bc1222c31c4f9
Cristi D <cristi.dnla@gmail.com>
2015-03-06 19:51:52 +0200

    Initial commit

---
+++ src/cd9list.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cd9list.h"
+
+void cd9list_foreach(CD9List *list, CD9Callback func, void *userData) 
+{
+    size_t index = 0;
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        (*func)(node->data, index, userData);
+        index++;
+    }
+}
+
+CD9Node *cd9list_createNode(void *data, size_t size) 
+{
+    CD9Node *node = malloc(sizeof(CD9Node));
+    if(node == NULL) { // Malloc failed.
+        return NULL;
+    }
+
+    node->data = data;
+
+    // Thus we can know when we reach the end of the list.
+    node->next = NULL;
+    node->size = size;
+
+    return node;
+}
+
+void *cd9list_get(void *self, size_t index)
+{
+    CD9List *list = (CD9List *)self;
+    
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        if(index == 0) {
+            return node->data;
+        } 
+        index--;
+    }
+    return NULL;
+}
+
+void cd9list_append(void *self, void *data)
+{
+    CD9List *list = (CD9List *)self;
+    CD9Node *newNode = cd9list_createNode(data, SIZE_ZERO);
+    CD9Node *lastNode;
+
+    if(list->nodes == NULL) {
+        // If the list is empty it means that the `nodes` memebers doesn't
+        // point to any node.
+        list->nodes = newNode;
+    }
+    else {
+        for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+            if(node->next == NULL) {
+                // That's the last node.
+                lastNode = node;
+            }    
+        }
+
+        // Now bind the new node to the last one.
+        lastNode->next = newNode;
+    }
+
+    // We should also increase the length of the list every time we add a new
+    // element.
+    list->length++;
+}
+/*
+void cd9list_appendCopy(void *self, void *data, size_t size)
+{
+    CD9List *list = (CD9List *)self;
+
+    void *holder = malloc(size);
+    memmove(holder, value, size);
+    
+
+}
+*/
+void cd9list_prepend(void *self, void *data)
+{
+    CD9List *list = (CD9List *)self;
+    CD9Node *newNode = cd9list_createNode(data, SIZE_ZERO);
+
+    if(list->nodes == NULL) {
+        list->nodes = newNode;
+    }
+    else {
+        // Used to backup the rest of the list.
+        CD9Node *tmp = list->nodes;
+
+        // Make the new node the first in the list.
+        list->nodes = newNode;
+        
+        // Bind the new node to the rest of the list.
+        list->nodes->next = tmp;
+    }
+
+    list->length++;
+}
+
+void cd9list_insert(void *self, size_t index, void *data)
+{
+    CD9List *list = (CD9List *)self;
+    if(index > list->length) {
+        return; // Not a valid index.
+    }
+    
+    if(index == list->length) {
+        list->append(list, data);
+        return;
+    }
+
+    CD9Node *tmp;
+    if(index == 0) {
+        CD9Node *newNode = cd9list_createNode(data, SIZE_ZERO);
+        
+        tmp = list->nodes;
+        list->nodes = newNode;
+        newNode->next = tmp;
+
+        list->length++;
+
+        return;
+    }
+
+    CD9Node *newNode = cd9list_createNode(data, SIZE_ZERO);
+    size_t i = 0;
+    
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        if(index - 1 == i) {
+            tmp = node->next;
+            node->next = newNode;
+            newNode->next = tmp;
+            list->length++;
+            return;
+        }
+        i++;
+    }
+}
+
+void cd9list_remove(void *self, size_t index)
+{
+    CD9List *list = (CD9List *)self;
+    if(index > list->length) {
+        return; // Not a valid index.
+    }
+
+    CD9Node *toRemove;
+    if(index == 0) {
+        toRemove = list->nodes;
+        list->nodes = toRemove->next;
+        cd9list_deleteNode(toRemove);
+    
+        list->length--;
+
+        return;
+    }
+
+    size_t i = 0;
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        if(i == index - 1) {
+            toRemove = node->next;
+            node->next = toRemove->next;
+            cd9list_deleteNode(toRemove);
+            
+            list->length--;
+        
+            return;
+        }   
+        i++;        
+    }
+}
+
+size_t cd9list_find(void *self, void *data)
+{
+    CD9List *list = (CD9List *)self;
+
+    size_t i = 0;
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        if(node->data == data) {
+            return i;
+        }
+        i++;
+    }
+
+    return -1;
+}
+
+CD9List *cd9list_createList()
+{
+    CD9List *list = malloc(sizeof(CD9List));
+    if(list == NULL) { // Malloc failed.
+        return NULL; 
+    }
+    
+    list->length = 0;
+    list->nodes  = NULL;
+
+    // Now bind the functions;
+    list->append         = cd9list_append;
+    list->prepend        = cd9list_prepend;
+    list->get            = cd9list_get;
+    list->insert         = cd9list_insert;
+    list->remove         = cd9list_remove;    
+    list->find           = cd9list_find;
+
+    return list;
+}
+
+void cd9list_deleteList(CD9List *list)
+{
+    CD9Node *phead = list->nodes;
+    CD9Node *tmp;
+
+    while(phead != NULL) {
+        tmp = phead->next;
+        free(phead);
+        phead = tmp;
+    }
+
+    free(list);
+}
+
+void cd9list_deleteNode(CD9Node *node)
+{
+    free(node);
+}
---
+++ tests/tests_cd9list.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include "minunit.h"
+#include "../src/cd9list.h"
+
+int tests_run = 0;
+  
+static char *test_createNode() 
+{
+    const char *data = "foo";
+    CD9Node *node = cd9list_createNode((void *)data, SIZE_ZERO);
+
+    mu_assert("Error allocating the list", node != NULL);
+    mu_assert("Data is not stored properly in a node", 
+        (char *)node->data == data );
+    mu_assert("Next node is not NULL", node->next == NULL);
+
+    return 0;
+}
+
+static char *test_createList()
+{
+    CD9List *list = cd9list_createList();
+
+    mu_assert("List is empty", list != NULL);
+    mu_assert("Default length is not 0", list->length == 0);
+    mu_assert("List nodes are not null", list->nodes == NULL);
+
+    return 0;
+}
+
+static char *test_append()
+{
+    const char *data = "foo";
+    CD9Node *lastNode;
+
+    CD9List *list = cd9list_createList();
+    list->append(list, (void *)data);
+
+    // Go to the las node.
+    for(CD9Node *node = list->nodes; node != NULL; node = node->next) {
+        if(node->next == NULL) {
+            // We have found the last node.
+            lastNode = node;
+        }
+    }
+   
+    mu_assert("The value was not appended properly",
+        (char *)lastNode->data == data);
+    mu_assert("The length of the list was not set properly",
+        list->length == 1);
+
+    return 0;
+}
+
+static char *test_prepend()
+{
+    const char *data = "foo";
+    CD9List *list = cd9list_createList();
+    list->prepend(list, (void *)data);
+
+    mu_assert("Node was not inserted properly", 
+        (char *)list->nodes->data == data);
+    mu_assert("The length of the list was not modified properly", 
+        list->length == 1);
+
+    return 0;
+}
+
+static char *test_get()
+{
+    const char *dataAtIndex0 = "foo";
+    const char *dataAtIndex1 = "bar";
+    const char *dataAtIndex2 = "baz";
+
+    CD9List *list = cd9list_createList();
+    
+    list->append(list, (void *)dataAtIndex0);
+    list->append(list, (void *)dataAtIndex1);
+    list->append(list, (void *)dataAtIndex2);
+
+    mu_assert("The index doesn't point to the right data",
+        (char *)list->get(list, 0) == dataAtIndex0);
+    
+    mu_assert("The index doesn't point to the right data",
+        (char *)list->get(list, 1) == dataAtIndex1);
+
+    mu_assert("The index doesn't point to the right data",
+        (char *)list->get(list, 2) == dataAtIndex2);
+
+    return 0;
+}
+
+static char *test_foreach()
+{
+    const char *data[] = {"foo", "bar", "baz"};
+    CD9List *list = cd9list_createList();
+
+    list->append(list, (void *)data[0]);
+    list->append(list, (void *)data[1]);
+    list->append(list, (void *)data[2]);
+    
+    return 0;
+
+}
+
+static char *all_tests() {
+    mu_run_test(test_createNode);
+    mu_run_test(test_createList);
+    mu_run_test(test_append);
+    mu_run_test(test_prepend);
+    mu_run_test(test_get);
+    return 0;
+}
+ 
+int main(int argc, char **argv) 
+{
+    char *result = all_tests();
+    
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+    
+    return result != 0;
+}    
